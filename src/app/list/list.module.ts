c192486aa941c72a434ff5e3fa06129f38cfd532
JozzHome <a.slobodyan@theomnicore.com>
2019-03-04 19:58:30 +0200

    add button + Pipe reverse

--- src/app/list/list.module.ts
+++ src/app/list/list.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListComponent } from './list.component';
 import { Routes, RouterModule } from '@angular/router';
+import { ReversePipe } from './countries.pipe';
 
 const routes: Routes = [
   {
@@ -12,7 +13,8 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [
-    ListComponent
+    ListComponent,
+    ReversePipe
   ],
   imports: [
     CommonModule,
